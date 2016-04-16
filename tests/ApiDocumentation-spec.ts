230d39ef79ac606e1b98557787c31deebbd0cf2f
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2016-04-16 22:10:04 +0200

    group ApiDocumentation tests

--- tests/ApiDocumentation-spec.ts
+++ tests/ApiDocumentation-spec.ts
@@ -5,53 +5,65 @@ import {Documentations} from './test-objects';
 
 describe('ApiDocumentation', () => {
 
-    it('should get operation\'s method and description given a type', done => {
+    describe('getting operations', () => {
 
-        var docs = new ApiDocumentation('', Documentations.classWithOperation);
+        it('should get operation\'s method and description given a type', done => {
 
-        var op = docs.getOperations('http://example.com/api#Class')
-            .then(op => {
-                expect(op[0].description).toBe('Gets the api#Class');
-                expect(op[0].method).toBe('GET');
-                done();
-            })
-            .catch(done.fail);
+            var docs = new ApiDocumentation('', Documentations.classWithOperation);
+
+            var op = docs.getOperations('http://example.com/api#Class')
+                .then(op => {
+                    expect(op[0].description).toBe('Gets the api#Class');
+                    expect(op[0].method).toBe('GET');
+                    done();
+                })
+                .catch(done.fail);
+        });
+
     });
 
-    it('should get properties for a given class type', done => {
+    describe('getting properties', () => {
 
-        var docs = new ApiDocumentation('', Documentations.classWithOperation);
+        it('should get properties for a given class type', done => {
 
-        var op = docs.getProperties('http://example.com/api#Class')
-            .then(props => {
-                expect(props.length).toBe(2);
-                done();
-            })
-            .catch(done.fail);
+            var docs = new ApiDocumentation('', Documentations.classWithOperation);
+
+            var op = docs.getProperties('http://example.com/api#Class')
+                .then(props => {
+                    expect(props.length).toBe(2);
+                    done();
+                })
+                .catch(done.fail);
+        });
+
     });
 
-    it('should return classes from documentation', done => {
+    describe('getting classes', () => {
 
-        var docs = new ApiDocumentation('', Documentations.classWithOperation);
+        it('should return classes from documentation', done => {
 
-        var classes = docs.getClasses()
-            .then(classes => {
-                expect(classes.length).toBe(1);
-                expect(classes[0].id).toBe('http://example.com/api#Class');
-                done();
-            })
-            .catch(done.fail);
-    });
+            var docs = new ApiDocumentation('', Documentations.classWithOperation);
 
-    it('should return selected class by @id', done => {
+            var classes = docs.getClasses()
+                .then(classes => {
+                    expect(classes.length).toBe(1);
+                    expect(classes[0].id).toBe('http://example.com/api#Class');
+                    done();
+                })
+                .catch(done.fail);
+        });
 
-        var docs = new ApiDocumentation('', Documentations.classWithOperation);
+        it('should return selected class by @id', done => {
 
-        var classes = docs.getClass('http://example.com/api#Class')
-            .then(clas => {
-                expect(clas.id).toBe('http://example.com/api#Class');
-                done();
-            })
-            .catch(done.fail);
+            var docs = new ApiDocumentation('', Documentations.classWithOperation);
+
+            var classes = docs.getClass('http://example.com/api#Class')
+                .then(clas => {
+                    expect(clas.id).toBe('http://example.com/api#Class');
+                    done();
+                })
+                .catch(done.fail);
+        });
+
     });
 });

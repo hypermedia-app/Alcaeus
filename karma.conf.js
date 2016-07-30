b2b84ca0908e4cc60407d793bdabf7f16e3b9d33
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2016-07-30 16:04:35 +0200

    reporters

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: process.env.TRAVIS ? ['dots'] : ['progress'],
+        reporters: ['progress'],
 
         plugins: ['karma-systemjs', 'karma-jasmine', 'karma-browserstack-launcher'],
 

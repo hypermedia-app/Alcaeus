be47acb6c7bf61c2d23f82439fd83ddba915bc17
tpluscode <tpluscode@users.noreply.github.com>
2016-08-15 22:11:15 +0200

    reformat karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,99 +1,97 @@
 // Karma configuration
 // Generated on Wed Oct 07 2015 12:43:21 GMT+0530 (IST)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['systemjs', 'jasmine'],
-
-    //plugins
-    plugins: ['karma-systemjs', 'karma-jasmine', 'karma-browserstack-launcher'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'tests/*-spec.ts',
-      'tests/*-specs.ts'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['systemjs', 'jasmine'],
+
+        //plugins
+        plugins: ['karma-systemjs', 'karma-jasmine', 'karma-browserstack-launcher'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'tests/*-spec.ts',
+            'tests/*-specs.ts'
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-    
-    browserStack: {
-      username: process.env.BROWSERSTACK_USERNAME,
-      accessKey: process.env.BROWSERSTACK_KEY
-    },
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // define browsers
-    customLaunchers: {
-      bs_firefox_mac: {
-        base: 'BrowserStack',
-        browser: 'firefox',
-        browser_version: '21.0',
-        os: 'OS X',
-        os_version: 'Mountain Lion'
-      },
-      bs_iphone5: {
-        base: 'BrowserStack',
-        device: 'iPhone 5',
-        os: 'ios',
-        os_version: '6.0'
-      }
-    },
 
-    browsers: ['bs_firefox_mac', 'bs_iphone5'],
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-    
-    systemjs: {
-             configFile: 'config.js',
-             serveFiles: [
-                 'src/**/*.ts',
-                 'tests/**/*.ts',
-                 'jspm_packages/**/*',
-                 'node_modules/**/*'
-             ]
-         }
-  })
+        browserStack: {
+            username: process.env.BROWSERSTACK_USERNAME,
+            accessKey: process.env.BROWSERSTACK_KEY
+        },
+
+        // define browsers
+        customLaunchers: {
+            bs_firefox_mac: {
+                base: 'BrowserStack',
+                browser: 'firefox',
+                browser_version: '21.0',
+                os: 'OS X',
+                os_version: 'Mountain Lion'
+            },
+            bs_iphone5: {
+                base: 'BrowserStack',
+                device: 'iPhone 5',
+                os: 'ios',
+                os_version: '6.0'
+            }
+        },
+
+        browsers: ['bs_firefox_mac', 'bs_iphone5'],
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        systemjs: {
+            configFile: 'config.js',
+            serveFiles: [
+                'src/**/*.ts',
+                'tests/**/*.ts',
+                'jspm_packages/**/*',
+                'node_modules/**/*'
+            ]
+        }
+    })
 };

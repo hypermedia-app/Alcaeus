31e6ed94baa2984fde17d295a874d88e2113e030
tpluscode <tpluscode@users.noreply.github.com>
2018-04-06 19:59:11 +0200

    use mocha reporter on travis

--- karma.conf.js
+++ karma.conf.js
@@ -121,7 +121,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: process.env.TRAVIS ? ['progress', 'saucelabs'] : ['progress', 'coverage'],
+        reporters: process.env.TRAVIS ? ['mocha', 'saucelabs'] : ['progress', 'coverage'],
 
 
         // web server port

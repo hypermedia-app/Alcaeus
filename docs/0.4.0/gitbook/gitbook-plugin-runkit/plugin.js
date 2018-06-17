f4b98024972d19c04e911ecf2834df5c745f2f2e
tpluscode <tpluscode@users.noreply.github.com>
2018-06-17 14:20:23 +0200

    v0.4.0

---
+++ docs/0.4.0/gitbook/gitbook-plugin-runkit/plugin.js
@@ -0,0 +1,24 @@
+require(["gitbook", "jQuery"], function(gitbook, $) {
+    function bindNotebook() {
+        var $pre = $(this);
+        var source = $pre.text();
+        var readOnly = $pre.hasClass('readonly');
+        var nodeVersion = $pre.data('node-version');
+
+        var $div = $('<div>');
+        $pre.replaceWith($div);
+
+        // var notebook = Tonic.createNotebook({
+        var notebook = RunKit.createNotebook({
+            element: $div.get(0),
+            source: source,
+            readOnly: readOnly,
+            nodeVersion: nodeVersion,
+            onLoad: function(){}
+        });
+    }
+
+    gitbook.events.bind("page.change", function() {
+        $('.pg-runkit').each(bindNotebook);
+    });
+});

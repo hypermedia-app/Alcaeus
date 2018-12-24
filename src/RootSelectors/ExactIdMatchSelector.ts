ce006f229d7314f9acbcb9fb99b8a3492859e1d0
tpluscode <tpluscode@users.noreply.github.com>
2018-12-24 08:57:23 +0100

    handle unescaped URIs in root selector

--- src/RootSelectors/ExactIdMatchSelector.ts
+++ src/RootSelectors/ExactIdMatchSelector.ts
@@ -3,6 +3,6 @@ import {IResourceGraph} from '../ResourceGraph';
 
 export default {
     selectRoot(resources: IResourceGraph, response: IHydraResponse) {
-        return resources[response.requestedUri];
+        return resources.get(response.requestedUri);
     },
 };

31e275e2a5187e27329d75824b0774b38ba8cbcd
tpluscode <tpluscode@users.noreply.github.com>
2019-08-09 14:38:22 +0200

    refactor: use simple proxy for vocabs

--- src/Vocabs.ts
+++ src/Vocabs.ts
@@ -2,6 +2,27 @@ type SchemaTerm = 'description' | 'title';
 
 type RdfsTerm = 'comment' | 'label' | 'range' | 'domain';
 
+interface NamespaceBuilder {
+    [key: string]: string;
+}
+
+const handler = {
+    apply: (target, thisArg, args) => target(args[0]),
+    get: (target, property) => target(property),
+}
+
+function namespace (baseIRI): NamespaceBuilder {
+    const builder = (term = '') => {
+        if (term === 'ns') {
+            return baseIRI
+        }
+
+        return `${baseIRI}${term}`
+    }
+
+    return typeof Proxy === 'undefined' ? builder : new Proxy(builder, handler)
+}
+
 export function Schema (term?: SchemaTerm) {
     return 'http://schema.org/' + (term || '')
 }
@@ -10,25 +31,8 @@ export function rdfs (term?: RdfsTerm) {
     return 'http://www.w3.org/2000/01/rdf-schema#' + (term || '')
 }
 
-const owlns = 'http://www.w3.org/2002/07/owl#'
-export const owl = {
-    ns: owlns,
+export const owl = namespace('http://www.w3.org/2002/07/owl#')
 
-    Nothing: owlns + 'Nothing',
-}
+export const rdf = namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 
-const rdfns = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-export const rdf = {
-    ns: rdfns,
-
-    Property: rdfns + 'Property',
-    type: rdfns + 'type',
-}
-
-const xsdns = 'http://www.w3.org/2001/XMLSchema#'
-export const xsd = {
-    ns: xsdns,
-
-    string: xsdns + 'string',
-    integer: xsdns + 'integer',
-}
+export const xsd = namespace('http://www.w3.org/2001/XMLSchema#')

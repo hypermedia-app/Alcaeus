d5b633b8b18c6b5a9786bce4c6aa57cb3aac7341
tpluscode <tpluscode@users.noreply.github.com>
2022-02-21 10:46:04 +0100

    fix: make factory functions generic

--- src/node.ts
+++ src/node.ts
@@ -1,15 +1,18 @@
 import formats from '@rdfjs/formats-common'
 import dataset from '@rdfjs/dataset'
+import { DatasetCore } from '@rdfjs/types'
 import * as Alcaeus from './index'
 import 'isomorphic-fetch'
 
-export function create(opts?: Partial<Parameters<typeof Alcaeus['create']>[0]>) {
-    return Alcaeus.create({
+export function create<D extends DatasetCore>(opts: Partial<Omit<Alcaeus.Init<D>, 'fetch' | 'Headers'>> = {}) {
+    const { datasetFactory, ...rest } = opts
+
+    return Alcaeus.create<D>({
         fetch,
         Headers,
         parsers: formats.parsers,
-        datasetFactory: dataset.dataset,
-        ...opts,
+        datasetFactory: datasetFactory || dataset.dataset as any,
+        ...rest,
     })
 }
 
--- src/web.ts
+++ src/web.ts
@@ -1,14 +1,17 @@
 import { parsers } from '@rdf-esm/formats-common'
 import dataset from '@rdfjs/dataset'
+import { DatasetCore } from '@rdfjs/types'
 import * as Alcaeus from './index'
 
-export function create(opts?: Partial<Parameters<typeof Alcaeus['create']>[0]>) {
-    return Alcaeus.create({
-        fetch: (input, init) => fetch(input, init),
+export function create<D extends DatasetCore>(opts: Partial<Omit<Alcaeus.Init<D>, 'fetch' | 'Headers'>> = {}) {
+    const { datasetFactory, ...rest } = opts
+
+    return Alcaeus.create<D>({
+        fetch,
         Headers,
         parsers,
-        datasetFactory: dataset.dataset,
-        ...opts,
+        datasetFactory: datasetFactory || dataset.dataset as any,
+        ...rest,
     })
 }
 
